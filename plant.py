56918637d9d09f86115410a92d25a26eca02f428
Mike Steder <steder@gmail.com>
2018-12-06 14:40:11 -0600

    Very simple data capture

--- plant.py
+++ plant.py
@@ -1,15 +1,40 @@
 #!/usr/bin/env python3
 
+import json
+
 from flask import Flask
+from flask import request
 app = Flask(__name__)
+
 
 DEBUG = True
 HOST = '0.0.0.0'
 PORT = 5000
 
+
+METRICS = []
+
+
 @app.route("/")
 def hello():
     return "Hello World!"
+
+
+@app.route("/metrics", methods=["GET"])
+def metrics():
+    return json.dumps({"data":METRICS[0:10]}), 200
+
+
+@app.route("/metrics", methods=["POST"])
+def new_metric():
+    moisture = request.args.get('m', None)
+    temperature = request.args.get('t', None)
+    light = request.args.get('l', None)
+    METRICS.append(
+        (moisture, temperature, light)
+    )
+    print(f"received metric: moisture {moisture}, temperature {temperature}, light {light}")
+    return "OK", 200
 
 
 if __name__=="__main__":
